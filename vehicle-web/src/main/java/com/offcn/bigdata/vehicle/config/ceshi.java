1497a2bc3e5cd9b9d76d9c226098512ca9766404
xiaoming <1194490058@qq.com>
2023-12-23 19:56:44 +0800

    dev的第二次修改操作

--- vehicle-web/src/main/java/com/offcn/bigdata/vehicle/config/ceshi.java
+++ vehicle-web/src/main/java/com/offcn/bigdata/vehicle/config/ceshi.java
@@ -2,6 +2,7 @@ package com.offcn.bigdata.vehicle.config;
 
 public class ceshi {
     public static void main(String[] args) {
-        System.out.println("dev的  -->  hello world!");
+        System.out.println("dev的    -->  hello world!");
+        System.out.println("依旧是dev1的");
     }
 }
